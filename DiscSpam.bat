47d28e72e251816add826ef026be844581150585
ChescoV1 <fcm12560@gmail.com>
2025-02-02 23:13:05 -0600

    DiscSpam.bat

---
+++ DiscSpam.bat
@@ -0,0 +1,94 @@
+@echo off
+chcp 65001
+cls
+goto loading
+
+:loading
+cls
+echo █    ████▄ ██   ██▄   ▄█    ▄     ▄▀         ▄▄▄▄▄   █ ▄▄  ██   █▀▄▀█ █▀▄▀█ ▄███▄   █▄▄▄▄ 
+echo █    █   █ █ █  █  █  ██     █  ▄▀          █     ▀▄ █   █ █ █  █ █ █ █ █ █ █▀   ▀  █  ▄▀ 
+echo █    █   █ █▄▄█ █   █ ██ ██   █ █ ▀▄      ▄  ▀▀▀▀▄   █▀▀▀  █▄▄█ █ ▄ █ █ ▄ █ ██▄▄    █▀▀▌  
+echo ███▄ ▀████ █  █ █  █  ▐█ █ █  █ █   █      ▀▄▄▄▄▀    █     █  █ █   █ █   █ █▄   ▄▀ █  █  
+echo     ▀         █ ███▀   ▐ █  █ █  ███                  █       █    █     █  ▀███▀     █   
+echo              █           █   ██                        ▀     █    ▀     ▀            ▀    
+echo             ▀                                               ▀                             
+ping localhost -n 2 >nul
+goto loading1
+
+:loading1
+cls
+echo █    ████▄ ██   ██▄   ▄█    ▄     ▄▀         ▄▄▄▄▄   █ ▄▄  ██   █▀▄▀█ █▀▄▀█ ▄███▄   █▄▄▄▄  
+echo █    █   █ █ █  █  █  ██     █  ▄▀          █     ▀▄ █   █ █ █  █ █ █ █ █ █ █▀   ▀  █  ▄▀  
+echo █    █   █ █▄▄█ █   █ ██ ██   █ █ ▀▄      ▄  ▀▀▀▀▄   █▀▀▀  █▄▄█ █ ▄ █ █ ▄ █ ██▄▄    █▀▀▌   
+echo ███▄ ▀████ █  █ █  █  ▐█ █ █  █ █   █      ▀▄▄▄▄▀    █     █  █ █   █ █   █ █▄   ▄▀ █  █   
+echo     ▀         █ ███▀   ▐ █  █ █  ███                  █       █    █     █  ▀███▀     █ ██ 
+echo              █           █   ██                        ▀     █    ▀     ▀            ▀     
+echo             ▀                                               ▀                              
+ping localhost -n 2 >nul
+goto loading2
+
+:loading2
+cls
+echo █    ████▄ ██   ██▄   ▄█    ▄     ▄▀         ▄▄▄▄▄   █ ▄▄  ██   █▀▄▀█ █▀▄▀█ ▄███▄   █▄▄▄▄     
+echo █    █   █ █ █  █  █  ██     █  ▄▀          █     ▀▄ █   █ █ █  █ █ █ █ █ █ █▀   ▀  █  ▄▀     
+echo █    █   █ █▄▄█ █   █ ██ ██   █ █ ▀▄      ▄  ▀▀▀▀▄   █▀▀▀  █▄▄█ █ ▄ █ █ ▄ █ ██▄▄    █▀▀▌      
+echo ███▄ ▀████ █  █ █  █  ▐█ █ █  █ █   █      ▀▄▄▄▄▀    █     █  █ █   █ █   █ █▄   ▄▀ █  █      
+echo     ▀         █ ███▀   ▐ █  █ █  ███                  █       █    █     █  ▀███▀     █ ██ ██ 
+echo              █           █   ██                        ▀     █    ▀     ▀            ▀        
+echo             ▀                                               ▀                                 
+ping localhost -n 2 >nul
+goto loading3
+
+:loading3
+cls
+echo █    ████▄ ██   ██▄   ▄█    ▄     ▄▀         ▄▄▄▄▄   █ ▄▄  ██   █▀▄▀█ █▀▄▀█ ▄███▄   █▄▄▄▄        
+echo █    █   █ █ █  █  █  ██     █  ▄▀          █     ▀▄ █   █ █ █  █ █ █ █ █ █ █▀   ▀  █  ▄▀        
+echo █    █   █ █▄▄█ █   █ ██ ██   █ █ ▀▄      ▄  ▀▀▀▀▄   █▀▀▀  █▄▄█ █ ▄ █ █ ▄ █ ██▄▄    █▀▀▌         
+echo ███▄ ▀████ █  █ █  █  ▐█ █ █  █ █   █      ▀▄▄▄▄▀    █     █  █ █   █ █   █ █▄   ▄▀ █  █         
+echo     ▀         █ ███▀   ▐ █  █ █  ███                  █       █    █     █  ▀███▀     █ ██ ██ ██ 
+echo              █           █   ██                        ▀     █    ▀     ▀            ▀           
+echo             ▀                                               ▀                                    
+ping localhost -n 2 >nul
+goto loadingdone
+
+:loadingdone
+cls
+echo    ▄▄▄▄▄   █ ▄▄  ██   █▀▄▀█ █▀▄▀█ ▄███▄   █▄▄▄▄     █    ████▄ ██   ██▄   ▄███▄   ██▄     ▄ 
+echo   █     ▀▄ █   █ █ █  █ █ █ █ █ █ █▀   ▀  █  ▄▀     █    █   █ █ █  █  █  █▀   ▀  █  █   █  
+echo ▄  ▀▀▀▀▄   █▀▀▀  █▄▄█ █ ▄ █ █ ▄ █ ██▄▄    █▀▀▌      █    █   █ █▄▄█ █   █ ██▄▄    █   █ █   
+echo  ▀▄▄▄▄▀    █     █  █ █   █ █   █ █▄   ▄▀ █  █      ███▄ ▀████ █  █ █  █  █▄   ▄▀ █  █  █   
+echo             █       █    █     █  ▀███▀     █           ▀         █ ███▀  ▀███▀   ███▀      
+echo              ▀     █    ▀     ▀            ▀                     █                      ▀   
+echo                   ▀                                             ▀                           
+ping localhost -n 2 >nul
+goto menu
+:menu
+cls
+echo ██   ▄█▄    ████▄ █▄▄▄▄   ▄          ▄▄▄▄▄   █ ▄▄  ██   █▀▄▀█ █▀▄▀█ ▄███▄   █▄▄▄▄ 
+echo █ █  █▀ ▀▄  █   █ █  ▄▀    █        █     ▀▄ █   █ █ █  █ █ █ █ █ █ █▀   ▀  █  ▄▀ 
+echo █▄▄█ █   ▀  █   █ █▀▀▌ ██   █     ▄  ▀▀▀▀▄   █▀▀▀  █▄▄█ █ ▄ █ █ ▄ █ ██▄▄    █▀▀▌  
+echo █  █ █▄  ▄▀ ▀████ █  █ █ █  █      ▀▄▄▄▄▀    █     █  █ █   █ █   █ █▄   ▄▀ █  █  
+echo    █ ▀███▀          █  █  █ █                 █       █    █     █  ▀███▀     █   
+echo   █                ▀   █   ██                  ▀     █    ▀     ▀            ▀    
+echo  ▀                                                  ▀                                      
+set /p webhook=Add the webhook to spam:
+set /p message=Add the message to spam:
+set /p delay=Put a delay delay on seconds(0=no):
+
+if %delay%==0 (
+    goto no_delay_spam
+) else (
+    goto delay_spam
+)
+
+
+:no_delay_spam
+curl -X POST -H "Content-Type: application/json" -d "{\"content\":\"%message%\"}" %webhook%
+echo Message sent 
+goto :no_delay_spam
+
+:delay_spam
+echo Sending Message...
+curl -X POST -H "Content-Type: application/json" -d "{\"content\":\"%message%\"}" %webhook%
+ping 127.0.0.1 -n %delay% > nul 
+goto :delay_spam
